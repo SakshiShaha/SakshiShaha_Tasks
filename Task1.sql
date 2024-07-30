27b7928de5e12af04fbb97c09f8f1817195510c8
SakshiShaha <85440109+SakshiShaha@users.noreply.github.com>
2024-07-30 09:27:25 +0530

    Add files via upload

---
+++ Task1.sql
@@ -0,0 +1,6 @@
+	CREATE DATABASE test;
+    use test;
+    create table students(S_id int,S_name varchar(30),S_age int);
+    insert into students values(1,"Ram",21);
+    insert into students values(2,"Shama",21);
+    select * from students;
